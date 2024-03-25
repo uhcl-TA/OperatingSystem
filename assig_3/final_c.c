547a91a66489198056091aa925819ac7066a33d0
akkpuvvada <akk.puvvada@gmail.com>
2024-03-25 15:43:39 -0500

    Adding the sample

--- assig_3/final_c.c
+++ assig_3/final_c.c
@@ -39,77 +39,21 @@ int main(int argc, char *argv[]) {
 
     // Main client loop
     while(1) {
+        // Read user input
+        printf("Enter message: ");
+        fgets(server_message, sizeof(server_message), stdin);
 
-        while (1) {
-        // Ask user for input
-        printf("What do you want to do?\n");
-        printf("1. Send request to server\n");
-        printf("2. Exit\n");
-        printf("3. Shutdown\n");
-        int choice;
-        scanf("%d", &choice);
+        // Send message to server
+        write(server_fd, server_message, strlen(server_message) + 1);
 
-        if (choice == 1) {
-            // Send request to server
-            printf("Enter system call number: ");
-            int systemCallNumber;
-            scanf("%d", &systemCallNumber);
-
-            printf("Enter number of parameters: ");
-            int parameterCount;
-            scanf("%d", &parameterCount);
-
-            char parameters[MAX_BUFFER_SIZE];
-            for (int i = 0; i < parameterCount; i++) {
-                printf("Enter parameter %d: ", i + 1);
-                scanf("%s", parameters);
-            }
-            // Format system call request
-            char request[MAX_BUFFER_SIZE];
-            sprintf(request, "%d,%d,%d,%d,%s", processID, systemCallNumber, parameterCount, strlen(parameters), parameters);
-
-            // Send request to server
-            write(serverFifo, request, sizeof(request));
-
-            // Read reply from server
-            char reply[MAX_BUFFER_SIZE];
-            read(clientFifo, reply, sizeof(reply));
-            int bytes_read = read(clientFifo, reply, sizeof(reply));
-            if (bytes_read > 0) {
-                // Print reply
-                printf("Reply from server: %s\n", reply);
-            }
-
-            // Print reply
-            // printf("Reply from server: %s\n", reply);
-        } else if (choice == 2) {
-            // Send exit system call to server
-            // (Code for sending exit request to server...)
-            break;
-        } else if (choice == 3) {
-            // Send shutdown system call to server
-            // (Code for sending shutdown request to server...)
+        // Check for exit command
+        if (strncmp(server_message, "exit", 4) == 0) {
             break;
         }
 
-
-
-
-        // // Read user input
-        // printf("Enter message: ");
-        // fgets(server_message, sizeof(server_message), stdin);
-
-        // // Send message to server
-        // write(server_fd, server_message, strlen(server_message) + 1);
-
-        // // Check for exit command
-        // if (strncmp(server_message, "exit", 4) == 0) {
-        //     break;
-        // }
-
-        // // Read server response
-        // read(client_fd, server_message, sizeof(server_message));
-        // printf("Server: %s\n", server_message);
+        // Read server response
+        read(client_fd, server_message, sizeof(server_message));
+        printf("Server: %s\n", server_message);
     }
 
     // Close FIFOs
